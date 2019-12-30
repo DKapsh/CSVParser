1488fbc83a9b03db06ab417719045a99178f768f
Daria Kapshuchenko <daria.kapshuchenko@apriorit.com>
2019-12-30 12:39:41 +0200

    Refactoring

--- gtest_test.cpp
+++ gtest_test.cpp
@@ -1,33 +1,21 @@
 #include <string>
+#include <vector>
 #include <gtest/gtest.h>
 namespace
 {
-    struct Row
+    std::vector<std::string> ParseRow(const std::string& input)
     {
-       std::string timestamp;
-       std::string ticker;
-    };
-    Row ParseRow(const std::string& input)
-    {
-        Row row;
-        row.timestamp = input.substr(0, input.find(','));
-        row.ticker = input.substr(input.find(',')+1, input.length() - row.timestamp.length() -1);
+        std::vector<std::string> row;
+        row.push_back(input.substr(0, input.find(',')));
+        row.push_back(input.substr(input.find(',')+1, input.length() - row[0].length() -1));
         return row;
-    }
-    bool operator== (const Row& lhv, const Row& rhv)
-    {     
-        if(lhv.timestamp == rhv.timestamp && lhv.ticker == rhv.ticker)
-        {
-            return true;
-        }
-        return false;
     }
 }
 
 TEST(CSVParser, ParseTwoValueInAString)
 {
     std::string inputString = "15051420,T";
-    Row row = {"15051420", "T"};
+    std::vector<std::string> row = {"15051420", "T"};
     EXPECT_EQ(row, ParseRow(inputString));
 }
 
