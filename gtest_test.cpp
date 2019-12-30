e19ac6996a5a6a678c0965cfe02745e43b52b052
Daria Kapshuchenko <daria.kapshuchenko@apriorit.com>
2019-12-30 12:32:09 +0200

    Green test

--- gtest_test.cpp
+++ gtest_test.cpp
@@ -4,15 +4,22 @@ namespace
 {
     struct Row
     {
-       
+       std::string timestamp;
+       std::string ticker;
     };
     Row ParseRow(const std::string& input)
     {
         Row row;
+        row.timestamp = input.substr(0, input.find(','));
+        row.ticker = input.substr(input.find(',')+1, input.length() - row.timestamp.length() -1);
         return row;
     }
     bool operator== (const Row& lhv, const Row& rhv)
     {     
+        if(lhv.timestamp == rhv.timestamp && lhv.ticker == rhv.ticker)
+        {
+            return true;
+        }
         return false;
     }
 }
@@ -20,7 +27,7 @@ namespace
 TEST(CSVParser, ParseTwoValueInAString)
 {
     std::string inputString = "15051420,T";
-    Row row;
+    Row row = {"15051420", "T"};
     EXPECT_EQ(row, ParseRow(inputString));
 }
 
