0a9237c3431cdc0182beb3e0e4563c39631e15e2
Daria Kapshuchenko <daria.kapshuchenko@apriorit.com>
2019-12-30 13:27:36 +0200

    Refactoring

--- gtest_test.cpp
+++ gtest_test.cpp
@@ -4,24 +4,21 @@
 #include <gtest/gtest.h>
 namespace
 {
-    std::vector<std::string> ParseRow(const std::string& input)
+    std::vector<std::string> SplitString(const std::string& input)
     {
         std::vector<std::string> row;
         int posPrev = 0;
-        int posNext = input.find(',', posPrev);
-        row.push_back(input.substr(posPrev, posNext));
-        posPrev = posNext + 1;
-        posNext = input.find(',', posPrev);
-        if(posNext == std::string::npos)
-        {
-            posNext == input.length();
-            row.push_back(input.substr(posPrev, posNext - posPrev));
-            return row;
+        int posNext = 0;
+        while(posNext < input.length())
+        {   
+            posNext = input.find(',', posPrev);
+            if(posNext == std::string::npos)
+            {
+                posNext == input.length();
+            }
+            row.push_back(input.substr(posPrev, posNext- posPrev));
+            posPrev = posNext + 1;
         }
-        row.push_back(input.substr(posPrev, posNext - posPrev));
-        posPrev = posNext + 1;
-        row.push_back(input.substr(posPrev, input.length() - posPrev));
-        printf("%s\n", row[1].data());
         return row;
     }
 }
@@ -30,12 +27,14 @@ TEST(CSVParser, ParseTwoValueInAString)
 {
     std::string inputString = "15051420,T";
     std::vector<std::string> row = {"15051420", "T"};
-    EXPECT_EQ(row, ParseRow(inputString));
+    EXPECT_EQ(row, SplitString(inputString));
 }
 
 TEST(CSVParser, ParseThreeValueInAString)
 {
     std::string inputString = "15051420,T,47.47";
     std::vector<std::string> row = {"15051420", "T", "47.47"};
-    EXPECT_EQ(row, ParseRow(inputString));
+    EXPECT_EQ(row, SplitString(inputString));
 }
+
+
