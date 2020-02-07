a4671998fa0b708f154b0a4111a10d04047e1f88
Daria Kapshuchenko <daria.kapshuchenko@apriorit.com>
2020-02-07 19:51:40 +0200

    Tests are RED

--- ParserUtils.cpp
+++ ParserUtils.cpp
@@ -59,10 +59,10 @@ std::string utils::FormedOutputData(const std::string& ticker, const std::vector
 {
     std::string tmp = ticker +", ";
     metrics::MetricsCalculator calculator;
-    tmp += std::to_string(calculator.GetMax(calculator.AskSubBid(inputData))) + ", ";
-    tmp += std::to_string(calculator.GetMin(calculator.AskSubBid(inputData)))+ ", ";
-    tmp += std::to_string(calculator.VolumeSum(inputData))+ ", ";
-    tmp += std::to_string(calculator.GetMaxDifferenceBetweenTimestamps(inputData))+ ", ";
+    tmp += std::to_string(calculator.GetMax(calculator.AskSubBid(inputData))) + ",";
+    tmp += std::to_string(calculator.GetMin(calculator.AskSubBid(inputData)))+ ",";
+    tmp += std::to_string(calculator.VolumeSum(inputData))+ ",";
+    tmp += std::to_string(calculator.GetMaxDifferenceBetweenTimestamps(inputData))+ ",";
     tmp += std::to_string(calculator.GetRatio(inputData))+'\n';
     return tmp;
 }
